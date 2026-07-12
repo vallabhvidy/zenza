cb11ef0cdae5e79c39c285d20ad88ac3f2340d26
Vallabh Vidyasagar <vallabhvidyasagar10@gmail.com>
2026-07-12 23:19:53 +0530

    add domain to allowed host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS
+      ? (process.env.VITE_ALLOWED_HOSTS === 'all' || process.env.VITE_ALLOWED_HOSTS === 'true'
+          ? true
+          : process.env.VITE_ALLOWED_HOSTS.split(','))
+      : ['localhost'],
+  },
 })
